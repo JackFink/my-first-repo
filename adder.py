bb79aa465eba766b39483e9042b1bf5f729ae6a4
JackFink <blusterbros@gmail.com>
2025-06-26 15:02:15 +0900

    aaaaaaa

--- adder.py
+++ adder.py
@@ -1,8 +1,9 @@
-def sum
-    a = 1
-    b = 2
-    i = a+b
 
-print (i)
-~
+def adder(a, b):
+    return a + b
+
+# 예시 실행
+if __name__ == "__main__":
+    result = adder(3, 5)
+    print(f"3 + 5 = {result}")
 
